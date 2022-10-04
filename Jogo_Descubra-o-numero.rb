7a7b63c6c9f27ed3f417485de8c6aad219eb42ea
Léia <leialimapretty@gmail.com>
2022-10-04 07:18:06 -0300

    Fechamento

---
+++ Jogo_Descubra-o-numero.rb
@@ -0,0 +1,34 @@
+novo_jogo = 's'
+
+while novo_jogo == 's'
+
+  puts 'Adivinhe o numero que estou pensando entre 1 e 100:'
+
+  seu_numero = gets.to_i
+
+  pc_numero = Random.rand 99 + 1
+  # puts pc_numero
+  
+  tentativas = 1
+
+  while pc_numero != seu_numero
+    if pc_numero > seu_numero
+      puts "O numero é maior que #{seu_numero}"
+    else
+      puts "O numero é menor que #{seu_numero}"
+    end
+    tentativas += 1
+
+    puts 'Tente novamente: '
+    seu_numero = gets.to_i
+  end
+
+  puts "Parabéns o numero era #{pc_numero}"
+  puts "Você usou #{tentativas} tentativas"
+
+  puts 'Você deseja jogar novamente? (s/n)'
+  novo_jogo = gets.chomp
+
+end
+
+puts 'Obrigado por jogar!'
